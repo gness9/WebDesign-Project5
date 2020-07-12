6cba69b689034f3efe4018c2d797e20a3a184751
Glenn Ness <gness9@ksu.edu>
2020-07-12 21:55:02 +0000

    Project 4 Initial

---
+++ server.js
@@ -0,0 +1,17 @@
+const handleRequest = require('./src/handle-request');
+const http = require('http');
+const port = 3000;
+
+/*
+var server = http.createServer(function(req, res) {
+  res.end("Hello, Web Browser!");
+});*/
+
+var server = http.createServer(handleRequest);
+
+server.listen(port, function(){
+  console.log("Server is listening on port " + port);
+  //console.log("CHECK");
+});
+
+//const pathToMimeType = require('./path-to-mime-type');
